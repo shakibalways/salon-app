e879d658313e104e03904e0891cfd5ab0eb23dad
S <shakibmohammad18@gmail.com>
2024-07-08 08:07:14 +0600

    Home page ui done

--- lib/Screen/Home Page/home_screen.dart
+++ lib/Screen/Home Page/home_screen.dart
@@ -21,68 +21,80 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
     return Scaffold(
       backgroundColor: Color(int.parse(RColors.homeColor)),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              children: [
-                RichText(
-                    text: TextSpan(
-                        style: const TextStyle(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 15,right: 25,top: 50
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                      text: TextSpan(
+                          style: const TextStyle(
+                              color: Colors.grey,
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold),
+                          children: <TextSpan>[
+                        const TextSpan(text: RTexts.titleText),
+                        TextSpan(
+                            text: RTexts.titleText1,
+                            style: TextStyle(
+                                color: Color(int.parse(RColors.colorCode)))),
+                      ])),
+                  Image.asset(
+                    RImages.notification,
+                    height: 30,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 420,
+                height: 50,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Color(int.parse(RColors.textfleidColor))),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          borderSide: const BorderSide(
                             color: Colors.grey,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold),
-                        children: <TextSpan>[
-                      const TextSpan(text: RTexts.titleText),
-                      TextSpan(
-                          text: RTexts.titleText1,
-                          style: TextStyle(
-                              color: Color(int.parse(RColors.colorCode)))),
-                    ])),
-                Image.asset(
-                  RImages.notification,
-                  height: 30,
-                )
-              ],
-            ),
-            SizedBox(
-              width: 420,
-              height: 50,
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: Color(int.parse(RColors.textfleidColor))),
-                child: TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: const BorderSide(
-                          color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      prefix: const Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: Icon(Icons.search),
-                      ),
-                      hintText: RTexts.fleidText,
-                      border: OutlineInputBorder(
-                          borderSide: const BorderSide(),
-                          borderRadius: BorderRadius.circular(25))),
+                        prefix: const Padding(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Icon(Icons.search),
+                        ),
+                        hintText: RTexts.fleidText,
+                        border: OutlineInputBorder(
+                            borderSide: const BorderSide(),
+                            borderRadius: BorderRadius.circular(25))),
+                  ),
                 ),
               ),
-            ),
-            Text(
-              RTexts.categori,
-              style: TextStyle(
-                  color: Color(
-                    int.parse(RColors.colorCode),
-                  ),
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: SizedBox(
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                RTexts.categori,
+                style: TextStyle(
+                    color: Color(
+                      int.parse(RColors.colorCode),
+                    ),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
                 height: 50,
                 child: ListView.builder(
                     scrollDirection: Axis.horizontal,
@@ -105,18 +117,21 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
                       );
                     }),
               ),
-            ),
-            Text(
-              RTexts.body,
-              style: TextStyle(
-                  color: Color(int.parse(RColors.colorCode)),
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                RTexts.body,
+                style: TextStyle(
+                    color: Color(int.parse(RColors.colorCode)),
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
                     Image.asset(RImages.productPercent),
@@ -124,24 +139,26 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
                   ],
                 ),
               ),
-            ),
-            const Text(
-              RTexts.selling,
-              style: TextStyle(
-                  color: Color(0xFF370C92),
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold),
-            ),
-            GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, mainAxisSpacing: 8),
-                itemCount: productSerial.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Container(
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                RTexts.selling,
+                style: TextStyle(
+                    color: Color(0xFF370C92),
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 40,
+                      crossAxisSpacing: 20),
+                  itemCount: productSerial.length,
+                  itemBuilder: (context, index) {
+                    return Container(
                       decoration:
                           BoxDecoration(color: Colors.white, boxShadow: [
                         BoxShadow(
@@ -158,7 +175,6 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
                             bottom: 60,
                             left: 35,
                             child: Column(
-
                               children: [
                                 Image.asset(productSerial[index].imagePath),
                                 Text(
@@ -172,7 +188,6 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
                                   height: 20,
                                 ),
                                 const Row(
-
                                   children: [
                                     Text("22 min"),
                                     CircleAvatar(
@@ -195,12 +210,13 @@ class _HomeScreenPageState extends State<HomeScreenPage> {
                           ),
                         ],
                       ),
-                    ),
-                  );
-                })
-          ],
+                    );
+                  })
+            ],
+          ),
         ),
       ),
+
     );
   }
 }
