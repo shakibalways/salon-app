30d6b251c1b14025ffb4891ccc87df1d7c874a96
S <shakibmohammad18@gmail.com>
2024-09-13 19:04:50 +0600

    Home Screen and Product Item and Payment Method Page Ui done

--- lib/Screen/Home Page/payment_method_page.dart
+++ lib/Screen/Home Page/payment_method_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:salon/utilits/constant/image_list.dart';
 
-
-
 class PaymentMethodPage extends StatefulWidget {
   const PaymentMethodPage({super.key});
 
